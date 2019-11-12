f3f7626ea9613e40f565581f9b3dd60887d6bc31
Kenpa <tchianphou@gmail.com>
2019-11-12 22:26:02 +0100

    fix android open notification

--- lib/src/adapters/NativeEventsReceiver.ts
+++ lib/src/adapters/NativeEventsReceiver.ts
@@ -1,9 +1,16 @@
-import { NativeModules, NativeEventEmitter, EventEmitter, EmitterSubscription } from 'react-native';
 import {
-  Registered, RegistrationError, RegisteredPushKit
-} from '../interfaces/NotificationEvents';
-import { Notification } from '../DTO/Notification';
-import { NotificationActionResponse } from '../interfaces/NotificationActionResponse';
+  NativeModules,
+  NativeEventEmitter,
+  EventEmitter,
+  EmitterSubscription
+} from "react-native";
+import {
+  Registered,
+  RegistrationError,
+  RegisteredPushKit
+} from "../interfaces/NotificationEvents";
+import { Notification } from "../DTO/Notification";
+import { NotificationActionResponse } from "../interfaces/NotificationActionResponse";
 
 export class NativeEventsReceiver {
   private emitter: EventEmitter;
@@ -11,32 +18,56 @@ export class NativeEventsReceiver {
     this.emitter = new NativeEventEmitter(NativeModules.RNEventEmitter);
   }
 
-  public registerRemoteNotificationsRegistered(callback: (event: Registered) => void): EmitterSubscription {
-    return this.emitter.addListener('remoteNotificationsRegistered', callback);
+  public registerRemoteNotificationsRegistered(
+    callback: (event: Registered) => void
+  ): EmitterSubscription {
+    return this.emitter.addListener("remoteNotificationsRegistered", callback);
   }
 
-  public registerPushKitRegistered(callback: (event: RegisteredPushKit) => void): EmitterSubscription {
-    return this.emitter.addListener('pushKitRegistered', callback);
+  public registerPushKitRegistered(
+    callback: (event: RegisteredPushKit) => void
+  ): EmitterSubscription {
+    return this.emitter.addListener("pushKitRegistered", callback);
   }
 
-  public registerRemoteNotificationReceived(callback: (notification: Notification) => void): EmitterSubscription {
-    return this.emitter.addListener('notificationReceived', (payload) => {
+  public registerRemoteNotificationReceived(
+    callback: (notification: Notification) => void
+  ): EmitterSubscription {
+    return this.emitter.addListener("notificationReceived", payload => {
       callback(new Notification(payload));
     });
   }
 
-  public registerPushKitNotificationReceived(callback: (event: object) => void): EmitterSubscription {
-    return this.emitter.addListener('pushKitNotificationReceived', callback);
+  public registerPushKitNotificationReceived(
+    callback: (event: object) => void
+  ): EmitterSubscription {
+    return this.emitter.addListener("pushKitNotificationReceived", callback);
   }
 
-  public registerRemoteNotificationOpened(callback: (notification: Notification, completion: () => void, actionResponse?: NotificationActionResponse) => void): EmitterSubscription {
-    return this.emitter.addListener('notificationOpened', (response, completion) => {
-      const action = response.action ? new NotificationActionResponse(response.action) : undefined
-      callback(new Notification(response.notification), completion, action);
-    });
+  public registerRemoteNotificationOpened(
+    callback: (
+      notification: Notification,
+      completion: () => void,
+      actionResponse?: NotificationActionResponse
+    ) => void
+  ): EmitterSubscription {
+    return this.emitter.addListener(
+      "notificationOpened",
+      (response, completion) => {
+        const action = response.action
+          ? new NotificationActionResponse(response.action)
+          : undefined;
+        callback(new Notification(response), completion, action);
+      }
+    );
   }
 
-  public registerRemoteNotificationsRegistrationFailed(callback: (event: RegistrationError) => void): EmitterSubscription {
-    return this.emitter.addListener('remoteNotificationsRegistrationFailed', callback);
+  public registerRemoteNotificationsRegistrationFailed(
+    callback: (event: RegistrationError) => void
+  ): EmitterSubscription {
+    return this.emitter.addListener(
+      "remoteNotificationsRegistrationFailed",
+      callback
+    );
   }
 }
